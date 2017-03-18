133b7f4136d8e36ec893534f8e917b76ce67fbc3
tvo6550 <tvo6550@gmail.com>
2017-03-17 22:29:43 -0700

    Add project files.

---
+++ AddOneToAnother/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AddOneToAnother._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+    <div class="row">
+        <h1>Add One Here!</h1>
+        <asp:TextBox ID ="inputBox" runat="server"></asp:TextBox>
+        <asp:Button ID ="computate" text="calculate" runat="server"/>
+        <asp:Label ID="result" Text="result" runat="server"></asp:Label>
+    </div>
+</div>
+
+</asp:Content>
---
+++ AddOneToAnother/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AddOneToAnother
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            computate.Click += new EventHandler(calculateBtn_Click);
+        }
+
+        protected void calculateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input = Convert.ToInt32(inputBox.Text);
+
+                AddOneReference.AddOneServiceClient addObj = new AddOneReference.AddOneServiceClient();
+
+                input = addObj.addOne(input);
+                result.Text = Convert.ToString(input);
+            }
+            catch(FormatException ee)
+            {
+                result.Text = "type in an integer";
+            }
+        }
+    }
+}
